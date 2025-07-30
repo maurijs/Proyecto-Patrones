9e2a9dd49536c3f3d9e07cafeedae7ecc0479828
Mauri Suarez <maurijo.suarez@gmail.com>
2025-07-30 00:24:43 -0300

    n

--- obj/Debug/net8.0/ProyectoFinal.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProyectoFinal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoFinal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ccc9ada5303058f99f7eb91e13e67447027e9bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoFinal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoFinal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
